bd7d7b1d10b05a5e91b5e64956ea0f491cd04bff
pperovic <paperovic@gmail.com>
2017-12-13 14:32:39 +0100

    First commit

---
+++ TV_film_Serija/TV.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TV_film_Serija
+{
+    public class TV
+    {
+
+        public string Naziv { get; set; }
+        public double Ocjena { get; set; }
+
+
+        //Unos metoda
+        public object Unos()
+        {
+            TV film = new TV();
+
+            Console.Write("Naziv: ");
+            film.Naziv = Console.ReadLine();
+            Console.Write("Ocjena: ");
+            film.Ocjena = double.Parse(Console.ReadLine());
+
+
+
+            return film;   
+
+
+           
+        }
+
+
+
+
+
+
+    }
+}
